667a711ce26fa36b9ac337771ca5c26908fa07ae
yccjackgx <yccjackgx@outlook.com>
2015-08-16 22:16:50 +0800

    first modify

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplication1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int main()
+{
+	printf("hello world first modify!!!");
+	system("pause");
+    return 0;
+}
+
